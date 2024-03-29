7ba6f86f8b50a66e14823d999b0e81afa4d5cdf3
Mateus Leonardo <m4teusmatos@gmail.com>
2024-03-28 23:50:53 -0300

    add function add cart in product screen

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -2,6 +2,8 @@ import { Image, Text, View } from "react-native"
 import { useLocalSearchParams } from "expo-router"
 import { Feather } from "@expo/vector-icons"
 
+import { useCartStore } from "@/stores/cart-store"
+
 import { PRODUCTS } from "@/utils/data/products"
 import { formatCurrency } from "@/utils/functions/format-currency"
 
@@ -9,9 +11,14 @@ import { Button } from "@/components/button"
 import { LinkButton } from "@/components/link-button"
 
 export default function Product() {
+  const cartStore = useCartStore()
   const { id } = useLocalSearchParams()
 
   const product = PRODUCTS.filter((item) => item.id === id)[0]
+
+  function handleAddToCart() {
+    cartStore.add(product)
+  }
 
   return (
     <View className="flex-1">
@@ -41,7 +48,7 @@ export default function Product() {
       </View>
 
       <View className="p-5 pb-8 gap-5">
-        <Button>
+        <Button onPress={handleAddToCart}>
           <Button.Icon>
             <Feather name="plus-circle" size={20} />
           </Button.Icon>
